d89c5da426a9d53e2b706dd0a2e5aa9a7189bab2
shekharsuyal <suyalshekhar94@gmail.com>
2023-03-10 19:48:44 +0530

    logo image change for quote_report

--- resources/views/partials/header_home.blade.php
+++ resources/views/partials/header_home.blade.php
@@ -3,7 +3,11 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="hero-logo">
-                    <a href="{{ route('home') }}"><img src="{{ asset('images/logo.png') }}" alt=""></a>
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('images/logo.png') }}" alt="" class="d-none d-lg-block">
+                        <img src="{{ asset('images/mobile_logo.png') }}" alt="" class="d-lg-none">
+                        
+                    </a>
                 </div>
             </div>
         </div>
