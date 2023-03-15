a54197d89334cc31bc815eff3b2cab0337161a7f
jagdisharma <jagdishcsharma29@gmail.com>
2023-03-15 12:55:58 +0530

    changes

--- resources/views/energybill/partials/header_home.blade.php
+++ resources/views/energybill/partials/header_home.blade.php
@@ -3,10 +3,8 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="hero-logo">
-                    <a href="{{ route('home') }}">
-                        <img src="{{ asset('energybill/images/logo.png') }}" alt="" class="d-none d-lg-block">
-                        
-                        
+                    <a href="{{ route('energy.home') }}">
+                        <img src="{{ asset('energybill/images/logo.png') }}" alt="" class="d-none d-lg-block">                        
                     </a>
                 </div>
             </div>
