ff80bb349313034342875df760ac7ac15ea54cea
jagdisharma <jagdishcsharma29@gmail.com>
2023-03-22 08:51:48 +0530

    changes

--- resources/views/solar/partials/top_header.blade.php
+++ resources/views/solar/partials/top_header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light top_header p-4">
     <div class="container">
         <div class="row">
-            <div class="col-10 offset-md-1 text-center text-white top_header_text">
+            <div class="col-xs-12 col-md-10 offset-md-1 text-center text-white top_header_text">
                 California Net Metering policies change on April 15th 2023. After this date, incentives for switching to solar will become less favorable for homeowners. Time is running out, check your eligibility now to get the maximum savings.
             </div>
         </div>
