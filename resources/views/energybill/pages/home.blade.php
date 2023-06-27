ebebc22e675ec7aacfb0c5181ab8704db0954e0d
gaurav singh karki <karkigauravsingh@gmail.com>
2023-06-27 09:37:52 +0530

    update text in home page

--- resources/views/energybill/pages/home.blade.php
+++ resources/views/energybill/pages/home.blade.php
@@ -24,7 +24,7 @@
                               <span class="city"> </span> </br> Residents Own Your Power By Going Solar
                            </h1>
                            <h1 class="fu7ycq-3 ieziWX">
-                              Enter basic home information to see if you qualify!
+                              Enter basic home information to see your free quote
                            </h1>
                         </div>
                      </div>
