d1dda4fa4b74d8dae7735df41df08c307ad712a4
gaurav singh karki <karkigauravsingh@gmail.com>
2023-08-11 16:06:53 +0530

    changes

--- lang/en/solar.php
+++ lang/en/solar.php
@@ -16,7 +16,7 @@ return [
     'estimate_for' => 'Who Is This Estimate For?',
     'privacy_text' => 'We Take Privacy Seriously.',
     'final_step' => 'Final Step!',
-    'final_step_text' => "You've Been Matched With Solar Experts In Your Area!",
+    'final_step_text' => "You've Been Matched With Solar Experts In Your Area",
     'tcpa_text' => 'By clicking the "View My Savings" button, you authorizeFinal www.powersolarsavings.com and up to 4 solar partners to call you and send you pre-recorded messages and text messages at the number you entered above, using an autodialer, with offers about their products or services, even if your phone number is on any national or state "Do Not Call" list. Message and data rates may apply. Your consent here is not based on a condition of purchase. You agree to the Terms of Use and Privacy Page.',
     'tcpa_label' => [
         'solar' => [
