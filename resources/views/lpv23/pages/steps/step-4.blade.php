6605dbe05e37f5340aab4dab5bf11c977e2b0b87
KarkiGaurav <karkigauravsingh@gmail.com>
2023-09-25 09:55:04 +0530

    increase home-type image size

--- resources/views/lpv23/pages/steps/step-4.blade.php
+++ resources/views/lpv23/pages/steps/step-4.blade.php
@@ -8,22 +8,22 @@
         <div class="custom-radio with-img borderd">
           <div class="custom-radio-item radioNext next-step" data-step="4" data-type_of_home="Single Family" >
             <input type="radio" name="type_of_home" class="" data-step="4" value="Single Family">
-            <span class="icon"><img loading="lazy" height="60" width="60" src="{{asset('_lpv23/images/single-family-outline.svg')}}"
+            <span class="icon"><img loading="lazy" style="scale: 1.6;" height="60" width="60" src="{{asset('_lpv23/images/single-family-outline.svg')}}"
                alt="">
            </span>Single Family</div>
           <div class="custom-radio-item radioNext next-step" data-step="4" data-type_of_home="3+ Stories">
             <input type="radio" name="type_of_home" class="" data-step="4" value="3+ Stories"><span
               class="icon">
-              <img loading="lazy" height="60" width="60" src="{{asset('_lpv23/images/multi-family-outline.svg')}}"
+              <img loading="lazy" height="60" width="60" style="scale: 1.6;"  src="{{asset('_lpv23/images/multi-family-outline.svg')}}"
                 alt="">
               </span>Multi Family</div>
           <div class="custom-radio-item radioNext next-step" data-step="4" data-type_of_home="2 Stories">
             <input type="radio" name="type_of_home" class="" data-step="4" value="2 Stories"><span
               class="icon">
-              <img loading="lazy" height="60" width="60" src="{{asset('_lpv23/images/mobile-home-outline.svg')}}"
+              <img loading="lazy" height="60" width="60" style="scale: 1.6;"  src="{{asset('_lpv23/images/mobile-home-outline.svg')}}"
                 alt=""></span>Mobile Home</div>
           <div class="custom-radio-item radioNext next-step" data-step="4" data-type_of_home="1 Story"><input type="radio" name="type_of_home" class="" data-step="4" value="1 Story"><span
-              class="icon"><img loading="lazy" height="60" width="60" src="{{asset('_lpv23/images/townhome-outline.svg')}}"
+              class="icon"><img loading="lazy" height="60" width="60" style="scale: 1.6;"  src="{{asset('_lpv23/images/townhome-outline.svg')}}"
                 alt=""></span>Townhome</div>
         </div>
         <span class="error-msg step_error_4" id="home-type-error" style="text-align: center;">&nbsp;</span>
